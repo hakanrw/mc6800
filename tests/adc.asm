314ed8fcbed8d84227afab2a01a9f3354ef7a709
Hakan Candar <hakancandar@proton.me>
2025-01-29 12:17:55 +0300

    add sbc/adc/sub tests

--- tests/adc.asm
+++ tests/adc.asm
@@ -1,4 +1,93 @@
-    LDAA #10     ; Load 10 into A
-    DECA         ; Decrement A (A = 9)
-    ADCA #1      ; Add 1 to A with carry
-    STAA $2000   ; Store result (10) in $2000
+; MC6800 ADC (Add With Carry) Comprehensive Test Program
+; Tests all flag conditions
+
+TEST_1:
+	CLC
+	LDX     #$2001        ; Test 1
+	LDAA    #$00
+	ADCA    #$01          ; N=0 Z=0 V=0 C=0
+
+	BMI     TEST_FAIL
+	BEQ     TEST_FAIL
+	BVS     TEST_FAIL
+	BCS     TEST_FAIL
+	CMPA	#$01
+	BNE     TEST_FAIL
+
+TEST_2:
+	SEC
+	LDX     #$2002        ; Test 2
+	LDAA    #$08
+	ADCA    #$08          ; N=0 Z=0 V=0 C=0
+
+	BMI     TEST_FAIL
+	BEQ     TEST_FAIL
+	BVS     TEST_FAIL
+	BCS     TEST_FAIL
+	CMPA    #$11
+	BNE     TEST_FAIL
+
+TEST_3:
+	SEC
+	LDX     #$2003        ; Test 3
+	LDAA    #$70
+	ADCA    #$0F          ; N=1 Z=0 V=1 C=0
+
+	BPL     TEST_FAIL
+	BEQ     TEST_FAIL
+	BVC     TEST_FAIL
+	BCS     TEST_FAIL
+	CMPA    #$80
+	BNE     TEST_FAIL
+
+TEST_4:
+	CLC
+	LDX     #$2004        ; Test 4
+	LDAA    #$10
+	ADCA    #$70          ; N=1 Z=0 V=1 C=0
+
+	BPL     TEST_FAIL
+	BEQ     TEST_FAIL
+	BVC     TEST_FAIL
+	BCS     TEST_FAIL
+	CMPA    #$80
+	BNE     TEST_FAIL
+
+TEST_5:
+	SEC
+	LDX     #$2005        ; Test 5
+	LDAA    #$80
+	ADCA    #$80          ; N=0 Z=0 V=1 C=1
+
+	BMI     TEST_FAIL
+	BEQ     TEST_FAIL
+	BVC     TEST_FAIL
+	BCC     TEST_FAIL
+	CMPA    #$01
+	BNE     TEST_FAIL
+
+TEST_6:
+	SEC
+	LDX     #$2006        ; Test 6
+	LDAA    #$FF
+	ADCA    #$FF          ; N=1 Z=0 V=0 C=1
+
+	BPL     TEST_FAIL
+	BEQ     TEST_FAIL
+	BVS     TEST_FAIL
+	BCC	TEST_FAIL
+	CMPA    #$FF
+	BNE     TEST_FAIL
+
+; Test Successful - Halt
+EXT_TEST_OK:
+	CLR     $2000
+	BRA END
+
+; Failure Handling
+TEST_FAIL:
+	LDAA	#$FF
+	STAA	,X
+	BRA END
+
+END:
--- tests/sub.asm
+++ tests/sub.asm
@@ -1,11 +1,10 @@
-; MC6800 SBC (Substract With Carry) Comprehensive Test Program
+; MC6800 SUB (Substract Without Carry) Comprehensive Test Program
 ; Tests all flag conditions
 
 TEST_1:
-	CLC
 	LDX     #$2001        ; Test 1
 	LDAA    #$01
-	SBCA    #$01          ; N=0 Z=1 V=0 C=0
+	SUBA    #$01          ; N=0 Z=1 V=0 C=0
 
 	BMI     TEST_FAIL
 	BNE     TEST_FAIL
@@ -15,23 +14,21 @@ TEST_1:
 	BNE     TEST_FAIL
 
 TEST_2:
-	SEC
 	LDX     #$2002        ; Test 2
-	LDAA    #$01
-	SBCA    #$01          ; N=1 Z=0 V=0 C=1
+	LDAA    #$1E
+	SUBA    #$0F          ; N=0 Z=0 V=0 C=0
 
-	BPL     TEST_FAIL
+	BMI     TEST_FAIL
 	BEQ     TEST_FAIL
 	BVS     TEST_FAIL
-	BCC     TEST_FAIL
-	CMPA    #$FF
+	BCS     TEST_FAIL
+	CMPA    #$0F
 	BNE     TEST_FAIL
 
 TEST_3:
-	CLC
 	LDX     #$2003        ; Test 3
 	LDAA    #$FF
-	SBCA    #$0F          ; N=1 Z=0 V=0 C=0
+	SUBA    #$0F          ; N=1 Z=0 V=0 C=0
 
 	BPL     TEST_FAIL
 	BEQ     TEST_FAIL
@@ -41,36 +38,33 @@ TEST_3:
 	BNE     TEST_FAIL
 
 TEST_4:
-	SEC
 	LDX     #$2004        ; Test 4
 	LDAA    #$10
-	SBCA    #$70          ; N=1 Z=0 V=0 C=1
+	SUBA    #$70          ; N=1 Z=0 V=0 C=1
 
 	BPL     TEST_FAIL
 	BEQ     TEST_FAIL
 	BVS     TEST_FAIL
 	BCC     TEST_FAIL
-	CMPA    #$9F
+	CMPA    #$A0
 	BNE     TEST_FAIL
 
 TEST_5:
-	SEC
 	LDX     #$2005        ; Test 5
 	LDAA    #$80
-	SBCA    #$01          ; N=0 Z=0 V=1 C=0
+	SUBA    #$01          ; N=0 Z=0 V=1 C=0
 
 	BMI     TEST_FAIL
 	BEQ     TEST_FAIL
 	BVC     TEST_FAIL
 	BCS     TEST_FAIL
-	CMPA    #$7E
+	CMPA    #$7F
 	BNE     TEST_FAIL
 
 TEST_6:
-	SEC
 	LDX     #$2006        ; Test 6
-	LDAA    #$81
-	SBCA    #$81          ; N=1 Z=0 V=0 C=1
+	LDAA    #$80
+	SUBA    #$81          ; N=1 Z=0 V=0 C=1
 
 	BPL     TEST_FAIL
 	BEQ     TEST_FAIL
